65bf5ce2a738bd8edf6aeb68f57bd63984d7b0f3
andreology27 <37252145+andreology@users.noreply.github.com>
2020-08-03 15:28:13 -0700

    wait.py holds demo on how to wait for elements to appear before acting on an element

--- web_auto/wait.py
+++ web_auto/wait.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #waiting for elements
 driver = webdriver.Firefox()
 driver.maximize_window()
-driver.get('http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
-source = driver.find_element_by_xpath('//*[@id="box3"]')
-dest = driver.find_element_by_xpath('//*[@id="box103"]')
-actions = ActionChains(driver)
-actions.drag_and_drop(source, dest).perform()
+driver.get('https://www.google.com/earth/')
+
+wait = WebDriverWait(driver, 10)
+launchEarthButton = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/header/div/nav[1]/ul[2]/li[2]/a/span/span')))
+launchEarthButton.click()
